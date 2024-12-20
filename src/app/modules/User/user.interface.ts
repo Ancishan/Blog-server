34d24f595786b57de85e67a6e2c84d523a2c0e49
Ancishan <shantodev027@gmail.com>
2024-12-20 13:01:45 +0800

    [UPDATE] user interface and model . user constant

--- src/app/modules/User/user.interface.ts
+++ src/app/modules/User/user.interface.ts
@@ -1,8 +1,21 @@
+import { Model, Types } from "mongoose";
+import USER_ROLES from "./user.const";
+
+export type TUserRoles = keyof typeof USER_ROLES;
 
 export interface IUser {
-    name: string,
-    email:string,
-    password:string,
-    role: 'admin' | 'user';
+    _id:Types.ObjectId;
+    name: string;
+    email:string;
+    password:string;
+    role: TUserRoles;
     isBlocked:boolean;
+}
+
+export interface UserModel extends Model<IUser> {
+    isUserExistsByEmail :(email: string) => Promise<IUser | null>;
+    isPasswordMatched: (
+        plainTextPassword: string,
+        hashedPassword: string
+    ) => Promise<boolean>
 }
--- src/app/modules/User/user.model.ts
+++ src/app/modules/User/user.model.ts
@@ -1,35 +1,35 @@
 import { model, Schema } from "mongoose";
-import { IUser } from "./user.interface";
+import { IUser, UserModel } from "./user.interface";
 import bcrypt from 'bcrypt'
 import config from "../../config";
 
-const userSchema = new Schema<IUser>(
+const userSchema = new Schema<IUser, UserModel>(
     {
-        name:{
-            type:String,
-            required:true
+        name: {
+            type: String,
+            required: true
         },
-        email:{
-            type:String,
-            required:true,
-            unique:true,
+        email: {
+            type: String,
+            required: true,
+            unique: true,
         },
-        password:{
-            type:String,
-            required:true,
+        password: {
+            type: String,
+            required: true,
         },
-        role:{
-            type:String,
-            enum:['user', 'admin'],
+        role: {
+            type: String,
+            enum: ['user', 'admin'],
             default: 'user',
         },
-        isBlocked:{
-            type:Boolean,
-            default:false,
+        isBlocked: {
+            type: Boolean,
+            default: false,
         },
     },
     {
-        timestamps:true,
+        timestamps: true,
     },
 );
 
@@ -45,10 +45,22 @@ userSchema.pre('save', async function (next) {
 
 // set after saving password
 
-userSchema.post('save', function(doc, next){
+userSchema.post('save', function (doc, next) {
     doc.password = '';
     next();
 });
 
+// email already exists
+userSchema.statics.isUserExistsByEmail = async (email: string) => {
+    return await User.findOne({ email });
+};
 
-export const User = model<IUser>('User', userSchema)
+// password Matched
+userSchema.statics.isPasswordMatched = async (
+    plainTextPassword: string,
+    hashedPassword: string
+) => {
+    return await bcrypt.compare(plainTextPassword, hashedPassword);
+};
+const User = model<IUser, UserModel>('User', userSchema)
+export default User
