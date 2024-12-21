b67240cbd3e1d61e179d9e5825f7d9af66b55e0d
Ancishan <shantodev027@gmail.com>
2024-12-21 14:54:30 +0800

    [Update] main route

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,23 +1,25 @@
 import { Router } from 'express';
 import { AuthRoutes } from '../modules/Auth/auth.route';
 import { UserRoutes } from '../modules/User/user.route';
+import { BlogRoutes } from '../modules/Blog/blog.routes';
+import { AdminRoutes } from '../modules/Admin/admin.routes';
 
 
 const router = Router();
 
 const moduleRoutes = [
-//   {
-//     path: '/blogs',
-//     route: BlogRoutes,
-//   },
+  {
+    path: '/blogs',
+    route: BlogRoutes,
+  },
   {
     path: '/auth',
     route: AuthRoutes,
   },
-//   {
-//     path: '/admin',
-//     route: AdminRoutes,
-//   },
+  {
+    path: '/admin',
+    route: AdminRoutes,
+  },
   {
     path: '/users',
     route: UserRoutes,
